56a8c643072ac546f169b78d137e6ff52a05438c
unknown <lia210350@gmail.com>
2020-10-20 03:19:47 -0500

    Added harvesting history

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 console.log("Starting extension");
-chrome.browserAction.onClicked.addListener(buttonClicked);
-let history = chrome.history.HistoryItem.id
-console.log(history);
 
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+chrome.history.search({text: '', maxResults: 100}, function(data) {
+    data.forEach(function(page) {
+        console.log(page.url);
+    });
+});
 
 function buttonClicked() {
 	console.log("Pulling history");
--- content.js
+++
@@ -1,3 +0,0 @@
-console.log("chrome extension started");
-// let history = chrome.history.HistoryItem.id
-// console.log(history);
